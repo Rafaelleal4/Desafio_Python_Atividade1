bfc0d26a64fa5f7db0fec15ed3558d8020b6d243
Rafael Leal <rafael.hls@puccampinas.edu.br>
2025-08-25 11:03:19 -0300

    Feat: Adição de dicas para o código da bomba e história introdutória

--- Jogo python.py
+++ Jogo python.py
@@ -1,14 +1,27 @@
-## Projeto de jogo em python
+import random
 
-
+print("Bem-vindo ao DC Heroe!\nEsse é um jogo de aventura em texto,\nonde suas escolhas determinarão o destino da sua jornada.")
 char_name = input("Digite o nome do seu herói: ")
-print("Bem-vindo ao jogo!")
 
+modo = 0
+while modo not in [1, 2]:
+    try:
+        print("Ateção: No modo Hardcore, a senha da bomba é gerada aleatoriamente e você tem apenas o seu HP de tentativas para desativá-la!")
+        modo = int(input("Escolha o modo de jogo: \n 1 - Fácil \n 2 - Hardcore\n"))
+    except ValueError:
+        print("Digite apenas 1 ou 2.")
 
+if modo == 1:
+    senha_correta = 3090
+else:
+    senha_correta = random.randint(1000, 9999)
+
+print(f"Você acorda em mais um dia da sua vida patética até que derrepente aparece uma mulher misteriosa em seu quarto, que aparenta ser uma heroína poderosa que usa magia. \n ZATANA: Olá, {char_name}, me chamo Zatana. Todos os hérois do planeta estão lutando com o Darkseid nesse momento, eu não tenho muito tempo, ja tenho que voltar e ajudar eles. Precisamos de você, o coringa plantou uma bomba em uma mina de escavação aqui perto!")
+print("ZATANA: Não consigo explicar mais, meu tempo aqui está acabando, você precisa ir impedir ele. Vou te dar alguns atributos dos melhores herois do mundo!")
 char_class = 0
 while char_class not in [1, 2, 3, 4]:
     try:
-        char_class = int(input("Escolha a classe do seu herói: \n 1 - Batman \n 2 - Superman \n 3 - Mulher Maravilha \n 4 - Flash"))
+        char_class = int(input("Escolha a classe do seu herói: \n 1 - Batman \n 2 - Superman \n 3 - Mulher Maravilha \n 4 - Flash\n"))
         if char_class not in [1, 2, 3, 4]:
             print("Classe inválida. Por favor, escolha um número entre 1 e 4.")
     except ValueError:
@@ -18,7 +31,6 @@ hp = 10
 strength = 10
 speed = 10
 smartness = 10
-
 
 if char_class == 1:
     print("Você escolheu Batman!")
@@ -48,81 +60,129 @@ elif char_class == 4:
     speed += 8
     smartness += 1
     char_class = "Flash"
-else:
-    print("Classe inválida! Escolha novamente.")
-    char_class = input("Escolha a classe do seu herói: \n 1 - Batman \n 2 - Superman \n 3 - Mulher Maravilha \n 4 - Flash \n ")
-
 
 print(f"Com os seguintes atributos: \n HP: {hp} \n Força: {strength} \n Velocidade: {speed} \n Inteligência: {smartness}")
-print("Sua missão é salvar o mundo do Coringa! \n Ele roubou uma bomba nuclear e pretende destruir uma mina de escavação! \n Você deve encontrar a bomba e desativá-la antes que seja tarde demais! ")
+print("Sua missão é salvar o mundo do Coringa! \nEle roubou uma bomba nuclear e pretende destruir uma mina de escavação! \nVocê deve encontrar a bomba e desativá-la antes que seja tarde demais!")
 print(f"Boa sorte, herói {char_name}!")
 
-while True:
-    # Se o personagem for inteligente o suficiente, pode escolher qualquer entrada
-    if smartness >= 14:
-        action = int(input("O que você quer fazer? \n 1 - Entrar na caverna pela frente  \n 2 - Entrar por cima  \n 3 - Desistir da missão\n "))
-        while action not in [1, 2, 3]:
-            action = int(input("Opção inválida. O que você quer fazer? \n 1 - Entrar na caverna pela frente  \n 2 - Entrar por cima  \n 3 - Desistir da missão\n "))
-    # Se não for inteligente o suficiente, só pode entrar pela frente ou desistir
-    else:
-        action = int(input("O que você quer fazer? \n 1 - Entrar na caverna pela frente  \n 3 - Desistir da missão\n "))
-        while action not in [1, 3]:
-            action = int(input("Opção inválida. O que você quer fazer? \n 1 - Entrar na caverna pela frente  \n 3 - Desistir da missão\n "))
+# Separa codigo da bomba
+encontrou_bomba = False
+acao_bomba = 0
 
-    # Morte do Superman
+while True:
+    if smartness >= 14:
+        action = int(input("O que você quer fazer? \n 1 - Entrar na caverna pela frente  \n 2 - Entrar por cima  \n 3 - Desistir da missão\n"))
+        while action not in [1, 2, 3]:
+            action = int(input("Opção inválida. O que você quer fazer? \n 1 - Entrar na caverna pela frente  \n 2 - Entrar por cima  \n 3 - Desistir da missão\n"))
+    else:
+        action = int(input("O que você quer fazer? \n 1 - Entrar na caverna pela frente  \n 2 - Desistir da missão\n"))
+        while action not in [1, 3]:
+            action = int(input("Opção inválida. O que você quer fazer? \n 1 - Entrar na caverna pela frente  \n 2 - Desistir da missão\n"))
+
     if action == 1 and char_class == "Superman":
         print("Você entrou na caverna pela frente e foi surpreendido por um grupo de capangas do Coringa com Kriptonita! "
               "Eles te capturaram e levaram para o covil do Coringa! "
               "Infelizmente, sua missão falhou!")
         break
 
-    # Continuação do jogo na situação 1 se não for o Superman
     elif action == 1 and char_class != "Superman":
         print("Você entrou na caverna pela frente e conseguiu derrotar os capangas do Coringa! Eles sabem que você está aqui.")
-        action = int(input("Você encontrou um inimigo! O que você quer fazer? \n 1 - Atacar \n 2 - Fugir\n "))
+        action = int(input("Você encontrou um inimigo! O que você quer fazer? \n 1 - Atacar \n 2 - Fugir\n"))
         if action == 1 and hp > 0:
             print("Você atacou o inimigo e conseguiu derrotá-lo! Porém, você sofreu 1 ponto de dano.")
+            print("Você encontrou um bilhete com numeros da senha da bomba")
+           
+            print(int(senha_correta/100))
+            
             hp -= 1
             print(f"Seu HP atual é {hp}.")
-            action = int(input("Você encontrou o Coringa e a bomba! O que você quer fazer? \n 1 - Atacar \n 2 - Fugir\n "))
+            action = int(input("Você encontrou o Coringa e a bomba! O que você quer fazer? \n 1 - Atacar \n 2 - Fugir\n"))
             if action == 1:
                 print("Você atacou o Coringa e conseguiu derrotá-lo! E você achou dois controles com ele, ou seja, tem uma bomba a mais para desativar.")
-                action = int(input("O que você quer fazer? \n 1 - Desativar a bomba 1 \n 2 - Desativar a bomba 2\n "))
-                if action == 1:
-                    print("Você tentou desativar a bomba 1, porém era uma bomba falsa! "
-                          "Infelizmente, a bomba 2 vai detonar e não há como desativá-la a tempo!")
-                elif action == 2:
-                    print("Você desativou a bomba 2 com sucesso! A outra bomba era falsa. A mina de escavação está a salvo!")
+                acao_bomba = int(input("O que você quer fazer? \n 1 - Desativar a bomba 1 \n 2 - Desativar a bomba 2\n"))
+                encontrou_bomba = True
             elif action == 2:
                 print("Você fugiu do combate e conseguiu escapar! Porém, a bomba vai detonar e não há como desativá-la a tempo! "
                       "Infelizmente, sua missão falhou!")
         break
 
-    # Continuação do jogo na situação 2
     elif action == 2:
         print("Você entrou na caverna por cima e conseguiu evitar os capangas do Coringa!")
-        action = int(input("Você encontrou um inimigo! O que você quer fazer? \n 1 - Atacar \n 2 - Fugir\n "))
+        action = int(input("Você encontrou um inimigo! O que você quer fazer? \n 1 - Atacar \n 2 - Fugir\n"))
         if action == 1 and hp > 0:
             print("Você atacou o inimigo e conseguiu derrotá-lo! Porém, você sofreu 1 ponto de dano.")
             hp -= 1
             print(f"Seu HP atual é {hp}.")
-            action = int(input("Você encontrou o Coringa e a bomba! O que você quer fazer? \n 1 - Atacar \n 2 - Fugir\n "))
+            action = int(input("Você encontrou o Coringa e a bomba! O que você quer fazer? \n 1 - Atacar \n 2 - Fugir\n"))
             if action == 1:
                 print("Você atacou o Coringa e conseguiu derrotá-lo! E você achou dois controles com ele, ou seja, tem uma bomba a mais para desativar.")
-                action = int(input("O que você quer fazer? \n 1 - Desativar a bomba 1 \n 2 - Desativar a bomba 2\n "))
-                if action == 1:
-                    print("Você tentou desativar a bomba 1, porém era uma bomba falsa! "
-                          "Infelizmente, a bomba 2 vai detonar e não há como desativá-la a tempo!")
-                elif action == 2:
-                    print("Você desativou a bomba 2 com sucesso! A outra bomba era falsa. A mina de escavação está a salvo!")
+                acao_bomba = int(input("O que você quer fazer? \n 1 - Desativar a bomba 1 \n 2 - Desativar a bomba 2\n"))
+                encontrou_bomba = True
             elif action == 2:
                 print("Você fugiu do combate e conseguiu escapar! Porém, a bomba vai detonar e não há como desativá-la a tempo! "
                       "Infelizmente, sua missão falhou!")
         break
 
-    # Desistência da missão
     elif action == 3:
         print("Você desistiu da missão e o Coringa conseguiu destruir a mina de escavação! "
-              "Infelizmente, sua missão falhou! \n Game Over!")
+              "Infelizmente, sua missão falhou! \nGame Over!")
         break
-    
+
+# Separa codigo da bomba
+if encontrou_bomba:
+    if acao_bomba == 1:
+        print("Você escolheu desarmar a bomba 1!")
+
+        
+        while hp > 0:
+            try:
+                tentativa = int(input("Digite a senha de 4 dígitos da bomba: "))
+                if len(str(tentativa).zfill(4)) != 4:  
+                    print("Senha inválida! Digite menos de 4 números (ex: 0423).")
+                    continue
+            except ValueError:
+                print("Senha inválida! Digite apenas números.")
+                continue
+
+            if tentativa == senha_correta:
+                print("Bomba 1 desarmada com sucesso!")
+                print("Mas a bomba 1 era falsa! Você perdeu tempo, e a bomba 2 (verdadeira) vai explodir!")
+                break
+            else:
+                hp -= 1
+                print(f"Senha incorreta! Tentativas restantes: {hp}")
+                if senha_correta > tentativa:
+                    print("A senha correta é maior do que a tentativa.")
+                else:
+                    print("A senha correta é menor do que a tentativa.")
+
+        if hp == 0:
+            print("Você errou todas as tentativas da bomba 1! Infelizmente, a bomba 2 (verdadeira) explodiu! Game Over.")
+        else:
+            print("A bomba 2 (verdadeira) explodiu! Infelizmente, sua missão falhou!")
+
+    elif acao_bomba == 2:
+        print("Você escolheu tentar desarmar a bomba 2!")
+        while hp > 0:
+            try:
+                tentativa = int(input("Digite a senha de 4 dígitos da bomba 2: "))
+                if len(str(tentativa).zfill(4)) != 4:
+                    print("Senha inválida! Digite menos de 4 números (ex: 1234).")
+                    continue
+            except ValueError:
+                print("Senha inválida! Digite apenas números.")
+                continue
+
+            if tentativa == senha_correta:
+                print(f"Você desativou a bomba 2 com sucesso! O mundo está a salvo graças a você, {char_name}! Você é um verdadeiro herói!")
+                break
+            else:
+                hp -= 1
+                print(f"Senha incorreta! Tentativas restantes: {hp}")
+                if senha_correta > tentativa:
+                    print("A senha correta é maior do que a tentativa.")
+                else:
+                    print("A senha correta é menor do que a tentativa.")
+
+        if hp == 0:
+            print("Você errou todas as tentativas! A bomba explodiu! Game Over.")
